8fbd5861aaea38fddc455221974ad15480dd2c6a
repo-visualizer <repo-visualizer@users.noreply.github.com>
2021-08-05 20:20:51 -0500

    Convert action to command line script

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,73 @@
-import { exec } from '@actions/exec'
-import * as core from '@actions/core'
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import { exec } from "@actions/exec"
+import * as core from "@actions/core"
+import React from "react"
+import ReactDOMServer from "react-dom/server"
 import fs from "fs"
 
 import { processDir } from "./process-dir.js"
 import { Tree } from "./Tree.tsx"
 
-const main = async () => {
-  core.info('[INFO] Usage https://github.com/githubocto/repo-visualizer#readme')
+const args = process.argv.slice(2)
 
-  core.startGroup('Configuration')
-  const username = 'repo-visualizer'
-  await exec('git', ['config', 'user.name', username])
-  await exec('git', [
-    'config',
-    'user.email',
+let outputFile = "./diagram.svg"
+let skipPush = true
+let skipCommit = true
+
+switch (args[0]) {
+  case "output":
+    outputFile = args[1]
+    break
+  default:
+    console.log(`Command ${args[0]} is not valid input.`)
+}
+
+const main = async () => {
+  core.info("[INFO] Usage https://github.com/githubocto/repo-visualizer#readme")
+
+  core.startGroup("Configuration")
+  const username = "repo-visualizer"
+  await exec("git", ["config", "user.name", username])
+  await exec("git", [
+    "config",
+    "user.email",
     `${username}@users.noreply.github.com`,
   ])
 
   core.endGroup()
 
-
-  const excludedPathsString = core.getInput("excluded_paths") || "node_modules,bower_components,dist,out,build,eject,.next,.netlify,.yarn,.git,.vscode,package-lock.json,yarn.lock"
-  const excludedPaths = excludedPathsString.split(",").map(str => str.trim())
-  const data = await processDir(`./`, excludedPaths);
+  const excludedPathsString =
+    core.getInput("excluded_paths") ||
+    "node_modules,bower_components,dist,out,build,eject,.next,.netlify,.yarn,.git,.vscode,package-lock.json,yarn.lock"
+  const excludedPaths = excludedPathsString.split(",").map((str) => str.trim())
+  const data = await processDir(`./`, excludedPaths)
 
   const componentCodeString = ReactDOMServer.renderToStaticMarkup(
     <Tree data={data} />
-  );
+  )
 
   const outputFile = core.getInput("output_file") || "./diagram.svg"
 
   await fs.writeFileSync(outputFile, componentCodeString)
 
-  await exec('git', ['add', outputFile])
-  const diff = await execWithOutput('git', ['status', '--porcelain', outputFile])
+  await exec("git", ["add", outputFile])
+  const diff = await execWithOutput("git", [
+    "status",
+    "--porcelain",
+    outputFile,
+  ])
   core.info(`diff: ${diff}`)
   if (!diff) {
-    core.info('[INFO] No changes to the repo detected, exiting')
+    core.info("[INFO] No changes to the repo detected, exiting")
     return
   }
 
-  exec('git', ['commit', '-m', "Repo visualizer: updated diagram"])
-  await exec('git', ['push'])
+  if (!skipCommit) {
+    exec("git", ["commit", "-m", "Repo visualizer: updated diagram"])
+  }
+
+  if (!skipPush) {
+    await exec("git", ["push"])
+  }
 
   console.log("All set!")
 }
@@ -62,12 +86,11 @@ function execWithOutput(command, args) {
           stderr: function (res) {
             core.info(res.toString())
             reject(res.toString())
-          }
-        }
+          },
+        },
       })
     } catch (e) {
       reject(e)
     }
   })
 }
-
